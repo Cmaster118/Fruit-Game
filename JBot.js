319e0d5f9dc53394eaeea350eda7ec05a6992e8f
Cmaster118 <jjwallace314@gmail.com>
2012-05-24 17:54:03 -0700

    Update JBot.js

--- JBot.js
+++ JBot.js
@@ -3,18 +3,52 @@ function new_game() {
 
 function make_move() {
    var board = get_board();
+   var xlength = get_board().length;
+   var ylength = get_board()[0].length;
+   var targetx = 0;
+   var targety = 0;
 
    // we found an item! take it!
    if (board[get_my_x()][get_my_y()] > 0) {
        return TAKE;
    }
 
-   var rand = Math.random() * 4;
+   // Searching for a target fruit
+   for(var i=0;i<xlength;i++){
+      for(var q=0;q<ylength;q++){
+         // Apples
+         if (board[i][q] === 1) {
+            targetx = i;
+            targety = q;
+         }
+         // Banana
+         if (board[i][q] === 2) {
+            targetx = i;
+            targety = q;
+         }
+         // Strawberrys
+         if (board[i][q] === 3) {
+            targetx = i;
+            targety = q;
+         }
+         // Watermelon
+         if (board[i][q] === 4) {
+            targetx = i;
+            targety = q;
+         }
+         // Oranges
+         if (board[i][q] === 5) {
+            targetx = i;
+            targety = q;
+         }
+      }
+  }
 
-   if (rand < 1) return NORTH;
-   if (rand < 2) return SOUTH;
-   if (rand < 3) return EAST;
-   if (rand < 4) return WEST;
+   // Movement
+   if (get_my_y() > targety) return NORTH;
+   if (get_my_y() < targety) return SOUTH;
+   if (get_my_x() < targetx) return EAST;
+   if (get_my_x() > targetx) return WEST;
 
    return PASS;
 }
