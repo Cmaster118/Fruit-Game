c130375092364f63743ed3e9918f0893c21d8bf7
Cmaster118 <jjwallace314@gmail.com>
2012-05-24 18:36:03 -0700

    Update JBot.js

--- JBot.js
+++ JBot.js
@@ -7,6 +7,12 @@ function make_move() {
    var ylength = get_board()[0].length;
    var targetx = 0;
    var targety = 0;
+   var distx = 100;
+   var disty = 100;
+   var distz = 100;
+   var minz = 100;
+   var minx = 0;
+   var miny = 0;
 
    // we found an item! take it!
    if (board[get_my_x()][get_my_y()] > 0) {
@@ -16,33 +22,27 @@ function make_move() {
    // Searching for a target fruit
    for(var i=0;i<xlength;i++){
       for(var q=0;q<ylength;q++){
-         // Apples
-         if (board[i][q] === 1) {
-            targetx = i;
-            targety = q;
-         }
-         // Banana
-         if (board[i][q] === 2) {
-            targetx = i;
-            targety = q;
-         }
-         // Strawberrys
-         if (board[i][q] === 3) {
-            targetx = i;
-            targety = q;
-         }
-         // Watermelon
-         if (board[i][q] === 4) {
-            targetx = i;
-            targety = q;
-         }
-         // Oranges
-         if (board[i][q] === 5) {
-            targetx = i;
-            targety = q;
+         // Checks each space for a fruit
+         if (board[i][q] != 0) {
+            // Finds the x and y distances from the robot
+            distx = Math.abs(i-get_my_x());
+            disty = Math.abs(q-get_my_y());
+            // Finds the direct distance from the robot
+            distz = Math.sqrt(distx*distx + disty+ disty);
+
+            // Saves only the SMALLEST distance
+            if (distz < minz) {
+               minz = distz;
+               // Saves the coordinates of the closest one
+               minx = i;
+               miny = q;
+            }
          }
       }
   }
+   // Takes the resulting one
+   targetx = minx;
+   targety = miny;
 
    // Movement
    if (get_my_y() > targety) return NORTH;
